0fd7726f4afbfd4d72c41eec8485245f2751d4c1
Lars Johansson <lars.johansson@gmail.com>
2023-09-11 12:32:31 +0200

    Update Layout.tsx
    testing CI/CD

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -79,7 +79,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>Azure AI</h1>
+                            <h1 className={styles.headerTitle}>MTC Oslo - Azure OpenAI Chat</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
